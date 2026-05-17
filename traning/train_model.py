0bd540cdcad49688ec7e24a46450cfb6316c19a9
Abhishek <abhisheksharm789@gmail.com>
2026-05-17 17:19:51 +0530

    Add files via upload
    Added CNN fire detection model

--- traning/train_model.py
+++ traning/train_model.py
@@ -1,1 +1,63 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
+# Image settings
+img_size = 128
+batch_size = 16
+
+# Dataset loader
+datagen = ImageDataGenerator(
+    rescale=1./255,
+    validation_split=0.2
+)
+
+train_data = datagen.flow_from_directory(
+    "../dataset",
+    target_size=(img_size, img_size),
+    batch_size=batch_size,
+    class_mode="binary",
+    subset="training"
+)
+
+val_data = datagen.flow_from_directory(
+    "../dataset",
+    target_size=(img_size, img_size),
+    batch_size=batch_size,
+    class_mode="binary",
+    subset="validation"
+)
+
+# CNN Model
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(128,128,3)),
+    MaxPooling2D(2,2),
+
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+
+    Flatten(),
+
+    Dense(128, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
+
+# Compile
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+# Train
+model.fit(
+    train_data,
+    validation_data=val_data,
+    epochs=5
+)
+
+# Save model
+model.save("../model/fire_model.h5")
+
+print("🔥 Model Trained Successfully")
